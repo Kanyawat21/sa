1ad4a700e4feedc93ef0d7ae30d28a4a1ec61898
n6teen <note.842545@gmail.com>
2023-10-15 20:46:07 +0700

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import { ProfilePage }    from "./screens/ProfilePage";
 import { ProfilePage2 }   from "./screens/ProfilePage2";
 
 
-//HELLO World โปรเจคโง่ๆ
+//HELLO World โปรเจคโง่ๆ  ทำไมคะ?
 
 const App: React.FC = () => {
   return (

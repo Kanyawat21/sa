9b482149e7a3464edeeb458756f7f1a05f9ece4e
Level69Magikrap <Balleyball005@gmail.com>
2023-10-16 17:46:38 +0700

    ball function

--- controller/ball.go
+++ controller/ball.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"time"
 	"github.com/gin-gonic/gin"
 	"github.com/n6teen/reactbasic/entity"
 )
@@ -43,4 +44,30 @@ func DeletePaymentByID(c *gin.Context){
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": id})
+}
+func CreatePayment(c *gin.Context) {
+	var payment entity.Payment
+	payment.Date = time.Now()
+	//var service entity.Service
+
+	if err := c.ShouldBindJSON(&payment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	p := entity.Payment{
+		Receipt:         payment.Receipt,
+		Date:            payment.Date,
+		ServiceID:       payment.ServiceID,
+		MemberID:        payment.MemberID,
+		MemberFirstName: payment.MemberFirstName,
+		MemberLastName:  payment.MemberLastName,
+	}
+
+	if err := entity.DB().Create(&p).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": p})
 }

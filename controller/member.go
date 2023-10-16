64cbddd4ef54d323e21e5f0ee19fefd6f3f4cdc4
Supachalita Polnong <142074041+n6teen@users.noreply.github.com>
2023-10-16 23:10:51 +0700

    Update member.go -d createReport

--- controller/member.go
+++ controller/member.go
@@ -42,19 +42,7 @@ func ListReview(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": review})
 }
 
-// =====================================================================Report
-func CreateReport(c *gin.Context) {
-	var report entity.Report
-	if err := c.ShouldBindJSON(&report); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	if err := entity.DB().Create(&report).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": report})
-}
+
 
 //=====================================================================User
 // POST /users
